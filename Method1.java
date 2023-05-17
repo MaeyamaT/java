ed2783b658fbb6736a5a14f7d47c78bac108eb5f
maeyama <Cralia.kzhr@gmail.com>
2023-05-17 13:03:19 +0900

    Method1.javaの作成

---
+++ Method1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Method1{
+	public static void main(String[] args){
+		//inputメゾッドの呼び出し
+		input();
+		//aggregateメゾッドの呼び出し
+		aggregate();
+　　//outputメゾッドの呼び出し
+		output();
+	}
+	//inputメゾッドの作成
+	public static void input(){
+		System.out.println("入力業務です");
+	}
+	//aggregateメゾッドの作成
+	public static void aggregate(){
+		System.out.println("集計業務です");
+	}
+	outptメゾッドの作成
+	public static void output(){
+		System.out.println("出力業務です");
+	}
+}
